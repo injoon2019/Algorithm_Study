ec14168e33594d3d1daf4432ad748c1d99f43d0b
Injun Son <ison@sfu.ca>
2021-01-28 18:18:13 +0900

    16장 완료

---
+++ 인준/16장/leetcode_implement-trie-prefix-tree2.py
@@ -0,0 +1,50 @@
+'''
+Problem Solving leetcode implement-trie-prefix-tree
+Author: Injun Son
+Date: January 28, 2021
+'''
+import sys
+import collections
+import heapq
+import functools
+import itertools
+import re
+import math
+import bisect
+from typing import *
+
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    # 단어 삽입
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+
